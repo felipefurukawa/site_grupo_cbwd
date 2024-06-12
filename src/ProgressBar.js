1ff6d277cde39377f87d317a16cfd1a85c08dfe4
felipefurukawa <felpissss@gmail.com>
2024-06-11 21:32:12 -0300

    queijo

--- src/ProgressBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { LinearProgress } from '@material-ui/core';
-
-function ProgressBar({ progress }) {
-  return <LinearProgress variant="determinate" value={progress} />;
-}
-
-export default ProgressBar;
